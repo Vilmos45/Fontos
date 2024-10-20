bb1806cbd3f2f99d9ae63b37cd24c6dc6509f1d0
Alich Vilmos <alich.vilmos.24f@szlgbp.hu>
2024-10-20 16:18:14 +0200

    8

--- aamoba.js
+++ aamoba.js
@@ -1,3 +1,22 @@
-var Kék = !Piros;
-var Piros = 0 ; 
+var Kék = 0;
+var Piros = 0; 
+var jatek_vege = 0;
 
+function clicked(s1) {
+    if(s1==clicked) {
+        let szin = 0
+        if (Piros + Kék < 9){
+            jatek_vege = 1
+        } 
+
+        if (Kék < Piros){
+            szin ++;
+            Kék ++;
+        } 
+        else{
+            Piros ++;
+            szin = Piros;
+        } 
+      return true;
+    } 
+  }
